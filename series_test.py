1d68c641b27f8456ea42b7dbfd50b61efae88a9e
Srirakshith Betageri <sbetageri111@gmail.com>
2017-06-04 17:20:20 +0530

    Multiply series by scalar

--- series_test.py
+++ series_test.py
@@ -10,4 +10,12 @@ d = {
 a = np.array(['a', 'b', 'c'])
 
 series = Series(15, [1, 2, 3])
-print series
+series[0] = 'Hello'
+series[3] = 'Hello'
+series[9] = 'Hello'
+
+series2 = Series(' World', [9, 8, 7])
+series2[1] = 9
+series = series.mul(2)
+
+print series + series2
